67cbc3f0d3631ee65e7a69ae301e1fa75543225e
Awajis <info@awajis.com>
2018-04-10 05:18:49 +0100

    index
    original

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	  <link rel="stylesheet" href="style.css">
+	<title>HNG - Stage 1</title>
+</head>
+<body>
+<h1>HNG Internship 2018</h1>
+
+ <?php
+date_default_timezone_set("Africa/Lagos");
+echo "Time in Nigeria: " . date("h:ia");
+?>
+</body>
+</html>
